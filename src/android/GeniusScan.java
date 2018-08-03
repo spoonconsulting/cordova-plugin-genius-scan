a560a9d9ebe11fab963ded9d6f90e25ef8dfb758
Mevin Dhunnooa <mdhunnooa@gmail.com>
2018-08-03 12:00:13 +0400

    removed thumbs on android

--- src/android/GeniusScan.java
+++ src/android/GeniusScan.java
@@ -87,58 +87,7 @@ public class GeniusScan extends CordovaPlugin {
         return false;
     }
 
-     private void generateThumbnail(Context context, String pathOfInputImage){
-        try
-        {
-            int inWidth = 0;
-            int inHeight = 0;
-            int dstWidth = 370;
-            int dstHeight = 370;
-
-            InputStream in = new FileInputStream(pathOfInputImage);
-
-            // decode image size (decode metadata only, not the whole image)
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inJustDecodeBounds = true;
-            BitmapFactory.decodeStream(in, null, options);
-            in.close();
-            in = null;
-
-            // save width and height
-            inWidth = options.outWidth;
-            inHeight = options.outHeight;
-
-            // decode full image pre-resized
-            in = new FileInputStream(pathOfInputImage);
-            options = new BitmapFactory.Options();
-            // calc rough re-size (this is no exact resize)
-            options.inSampleSize = Math.max(inWidth/dstWidth, inHeight/dstHeight);
-            // decode full image
-            Bitmap roughBitmap = BitmapFactory.decodeStream(in, null, options);
-
-            // calc exact destination size
-            Matrix m = new Matrix();
-            RectF inRect = new RectF(0, 0, roughBitmap.getWidth(), roughBitmap.getHeight());
-            RectF outRect = new RectF(0, 0, dstWidth, dstHeight);
-            m.setRectToRect(inRect, outRect, Matrix.ScaleToFit.CENTER);
-            float[] values = new float[9];
-            m.getValues(values);
-
-            // resize bitmap
-            Bitmap resizedBitmap = Bitmap.createScaledBitmap(roughBitmap, (int) (roughBitmap.getWidth() * values[0]), (int) (roughBitmap.getHeight() * values[4]), true);
-
-            // save image
-            File file = new File(pathOfInputImage);
-            String filename = file.getName();
-            FileOutputStream out = new FileOutputStream(new File(context.getFilesDir(), "thumb_" + filename).getAbsolutePath());
-            resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 80, out);
-
-        }
-        catch (Exception e)
-        {
-            Log.e("Image", e.getMessage(), e);
-        }
-    }
+     
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -147,7 +96,6 @@ public class GeniusScan extends CordovaPlugin {
             if( resultCode == Activity.RESULT_OK)
             {
                 String enhancedImageFilePath = scanContainer.getEnhancedImage().getAbsolutePath(null);
-                generateThumbnail(this.cordova.getActivity().getApplicationContext(), enhancedImageFilePath);
                 Uri enhancedImageFileUri = Uri.fromFile(new File(enhancedImageFilePath));
                 PluginResult result = new PluginResult(PluginResult.Status.OK, enhancedImageFileUri.toString());
                 result.setKeepCallback(true);
